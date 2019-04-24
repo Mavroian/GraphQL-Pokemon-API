7ad5819fe7cfc6e7c3b640b5e2dae546defc708d
Mavroian and Min Kyeonghun <mavflorin+sunnyziru@gmail.com>
2019-04-24 15:35:33 +0900

    BR Finished

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const schema = buildSchema(`
     type: String
     damage:Int
   }
-  type AttactWrap {
+  type AttackWrap {
     fast:[Attack]
     special:[Attack]
   }
@@ -41,15 +41,53 @@ const schema = buildSchema(`
     evolutions:[SimplePoke]
     maxCP:Int
     maxHP:Int
-    attacks: AttactWrap
+    attacks: AttackWrap
   }
+  
 
 
   type Query {
     Pokemons: [Pokemon]
     Pokemon(name: String!): Pokemon
+    PokemonWithAttackName(name: String!): [Pokemon]
     Types: [String]
-    Attacks: AttactWrap
+    Type(name: String!) : [Pokemon]
+    Attacks: AttackWrap
+    AttacksWithType(type:String!): [Attack]
+    
+  }
+
+  input SizesInput {
+    minimum:String
+    maximum:String
+  }
+  input AttackInput {
+    name: String
+    type: String
+    damage:Int
+  }
+  input PokemonInput {
+    id: String
+    name: String!
+    classification: String
+    types:[String]
+    resistant:[String]
+    weaknesses:[String]
+    weight: SizesInput
+    height: SizesInput
+    fleeRate:Float
+  }
+
+  type Mutation {
+    DeleteAttack(name:String!): [Attack]
+    AddType(newType: String!): [String]
+    ModifyType(old:String!,new:String!): [String]
+    DeleteType(old:String!): String
+    AddAttack(type: String!, input: AttackInput): [Attack]
+    ModifyAttack(name: String!, input: AttackInput): Attack
+    AddPokemon(input: PokemonInput): [Pokemon]
+    ModifyPokemon(id: String, input: PokemonInput): Pokemon
+    DeletePokemon(id:String):[Pokemon]
   }
 `);
 
@@ -61,11 +99,101 @@ const root = {
   Pokemon: (request) => {
     return data.pokemon.find((pokemon) => pokemon.name === request.name);
   },
+  PokemonWithAttackName: (request) => {
+    return data.pokemon.filter((pokemon) => {
+      return (
+        pokemon.attacks.fast
+          .map((attack) => {
+            return attack.name;
+          })
+          .includes(request.name) ||
+        pokemon.attacks.special
+          .map((attack) => {
+            return attack.name;
+          })
+          .includes(request.name)
+      );
+    });
+  },
   Types: () => {
     return data.types;
   },
+  Type: (request) => {
+    console.log(data.pokemon);
+    return data.pokemon.filter((pokemon) => {
+      return pokemon.types.includes(request.name);
+    });
+  },
   Attacks: () => {
     return data.attacks;
+  },
+  AttacksWithType: (request) => {
+    return data.attacks[request.type];
+  },
+  AddType: (request) => {
+    data.types.push(request.newType);
+    return data.types;
+  },
+  ModifyType: (request) => {
+    if (data.types.includes(request.old)) {
+      data.types[data.types.indexOf(request.old)] = request.new;
+      return data.types;
+    }
+    return ["error"];
+  },
+  DeleteType: (request) => {
+    if (data.types.includes(request.old)) {
+      return data.types.splice(data.types.indexOf(request.old), 1)[0];
+    }
+    return `you dont have ${request.old}.`;
+  },
+  AddAttack: (request) => {
+    const { type, input } = request;
+    data.attacks[type].push(input);
+    return data.attacks[type];
+  },
+  DeleteAttack: (request) => {
+    const { name } = request;
+    for (const type in data.attacks) {
+      for (let i = 0; i < data.attacks[type].length; i++) {
+        if (data.attacks[type][i].name === name) {
+          return data.attacks[type].splice(i, 1);
+        }
+      }
+    }
+  },
+  ModifyAttack: (request) => {
+    const { name, input } = request;
+    for (const type in data.attacks) {
+      for (const attack of data.attacks[type]) {
+        if (attack.name === name) {
+          Object.assign(attack, input);
+          return attack;
+        }
+      }
+    }
+  },
+  AddPokemon: (request) => {
+    const { input } = request;
+    data.pokemon.push(input);
+    return data.pokemon;
+  },
+  ModifyPokemon: (request) => {
+    const { id, input } = request;
+    for (const pokemon of data.pokemon) {
+      if (pokemon.id === id) {
+        Object.assign(pokemon, input);
+        return pokemon;
+      }
+    }
+  },
+  DeletePokemon: (request) => {
+    const { id } = request;
+    for (const pokemon in data.pokemon) {
+      if (data.pokemon[pokemon].id === id) {
+        return data.pokemon.splice(pokemon, 1);
+      }
+    }
   },
 };
 
